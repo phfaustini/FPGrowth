58c0122299a7b9ce339844b24731ee34395c6ec2
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-03-09 12:38:14 -0300

    Fixed Control.Parallel import error.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+    fib
     ) where
 
+import Control.Parallel
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = n1 `par` (n1 + n2) 
+    where
+    n1 = fib (n - 1)
+    n2 = fib (n - 2)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ fib 36
