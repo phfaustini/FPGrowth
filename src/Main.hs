e3668d76916519fe8b1a91358e0823418f425180
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-03-28 18:19:48 -0300

    Started reading file.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+-- https://stackoverflow.com/questions/11229854/how-can-i-parse-the-io-string-in-haskell
+
 import Control.Parallel
 import MyTree
 
@@ -12,5 +14,17 @@ fib n = n1 `par` n2 `pseq` (n2 + n1)
     n2 = fib (n - 2)
 
 
+
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
+
+parseTransactions lines output
+    | null lines = output
+    | otherwise = parseTransactions (tail lines) (words $ head lines) ++ output
+
+
 main::IO ()
-main = print (fib 36)
+main = do
+    lines <- readLines "input/transactions2.txt"
+    print lines
