9de3bc271d423200df24fbcad77f475881b5aef1
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-03-29 18:42:07 -0300

    Counting frequency of each item.

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ import Control.Parallel
 import TransactionsReader
 import FPTree
 import FPGrowth -- minsup
-
+import qualified Data.Map as Map
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile -- ["4 3 1",    "12 3 1",    "23 34 9",   "2","90 1",    "5 1 12"]
@@ -18,4 +18,5 @@ main = do
     fileContent <- readLines filepath -- "input/transactions2.txt"
     let transactions = map words fileContent
     print transactions
+    print (countItems transactions (Map.fromList []) )
     print minsup
--- src/TransactionsReader.hs
+++ src/TransactionsReader.hs
@@ -1,13 +1,24 @@
 module TransactionsReader where
     
--- | Count how many times each item appears in all transactions
-{-
-countItems transactions output
-    | null transactions = output
-    | otherwise = countItems (tail transactions) (countRow $ head transactions)
-    where
-        -- Se elemento nao esta em output, output[elemento] = 1, senao output[elemento] += 1
-        countRow transaction
-        | null transaction = 
+import qualified Data.Map as Map
 
--}
+fromJust Nothing  = 0
+fromJust (Just x) = x
+
+-- | Count how many times each item appears in all transactions
+countItems transactions counting
+    | null transactions = counting
+    | otherwise = countItems (tail transactions) (updateCounting (head transactions) counting)
+    where
+        -- Se elemento nao esta em counting, counting[elemento] = 1, senao counting[elemento] += 1
+        updateCounting transaction counting
+            | null transaction = counting
+            | otherwise = updateCounting (tail transaction) (updateElement (head transaction) counting)
+            where
+                updateElement element counting = Map.insert element (check element counting + 1) counting
+                check element counting
+                        | Map.lookup element counting == Nothing = 0
+                        | otherwise = fromJust (Map.lookup element counting)
+
+
+
