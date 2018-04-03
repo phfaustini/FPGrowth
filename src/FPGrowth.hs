4177c61ed79951df73fa9019b548a02fefb44e71
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-04-03 17:07:39 -0300

    Fixed bug preventing adding transactions in succession.

--- src/FPGrowth.hs
+++ src/FPGrowth.hs
@@ -4,20 +4,11 @@ import FPTree
 
 minsup = 0.4
 
-
-{-
-insertTransaction :: [String] -> FPNode -> FPNode -> FPNode
-insertTransaction transaction currentNode outpuTtree
-    | null transaction = outpuTtree
-    | otherwise = insertTransaction (tail transaction) (head (fpchildren inserted)) (FPNode (fpitem outpuTtree) (fpcount outpuTtree) (fpchildren inserted))
-    where
-        inserted = insertNode (head transaction) currentNode
--}
-
 insertTransaction :: [String] -> FPNode -> FPNode
 insertTransaction transaction currentNode 
     | null transaction = currentNode
-    | otherwise = FPNode (fpitem currentNode) (fpcount currentNode) [insertTransaction (tail transaction) newElement]
+    | hasChild (head transaction) (fpchildren currentNode) = FPNode (fpitem currentNode) (fpcount currentNode) [insertTransaction (tail transaction) newElement]
+    | otherwise = FPNode (fpitem currentNode) (fpcount currentNode) (fpchildren currentNode ++ [insertTransaction (tail transaction) newElement])
     where 
         inserted = insertNode (head transaction) currentNode
         newElement = head (fpchildren inserted)
