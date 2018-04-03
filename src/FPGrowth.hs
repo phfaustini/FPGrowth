e41efb63368d028111a6d02d45155c614103472c
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-04-03 16:58:59 -0300

    Starting building FPTree.

--- src/FPGrowth.hs
+++ src/FPGrowth.hs
@@ -1,3 +1,23 @@
 module FPGrowth where
 
+import FPTree
+
 minsup = 0.4
+
+
+{-
+insertTransaction :: [String] -> FPNode -> FPNode -> FPNode
+insertTransaction transaction currentNode outpuTtree
+    | null transaction = outpuTtree
+    | otherwise = insertTransaction (tail transaction) (head (fpchildren inserted)) (FPNode (fpitem outpuTtree) (fpcount outpuTtree) (fpchildren inserted))
+    where
+        inserted = insertNode (head transaction) currentNode
+-}
+
+insertTransaction :: [String] -> FPNode -> FPNode
+insertTransaction transaction currentNode 
+    | null transaction = currentNode
+    | otherwise = FPNode (fpitem currentNode) (fpcount currentNode) [insertTransaction (tail transaction) newElement]
+    where 
+        inserted = insertNode (head transaction) currentNode
+        newElement = head (fpchildren inserted)
--- src/FPTree.hs
+++ src/FPTree.hs
@@ -2,13 +2,13 @@ module FPTree where
 
 data FPNode = FPNode { fpitem :: String, fpcount :: Integer, fpchildren :: [FPNode]} deriving (Show, Eq)
 
-root = FPNode "null" (-1) []
+{-root = FPNode "null" 0 []
 e = FPNode "e" 1 []
 d = FPNode "d" 1 [e]
 c = FPNode "c" 1 [d]
 b = FPNode "b" 1 []
 a = FPNode "a" 1 [b,c]
-
+-}
 
 hasChild :: String -> [FPNode] -> Bool
 hasChild key children
@@ -26,6 +26,5 @@ incrementChild key children output
 
 insertNode :: String -> FPNode -> FPNode
 insertNode key tree
-    -- | key == "null" = FPNode "null" (-1) [] -- Tree is created
     | hasChild key (fpchildren tree) = FPNode (fpitem tree) (fpcount tree) (incrementChild key (fpchildren tree) []) -- Tree has this child
-    | otherwise = FPNode (fpitem tree) (fpcount tree) (FPNode key 0 [] : fpchildren tree) -- Add key to tree's children
+    | otherwise = FPNode (fpitem tree) (fpcount tree) (FPNode key 1 [] : fpchildren tree) -- Add key to tree's children
