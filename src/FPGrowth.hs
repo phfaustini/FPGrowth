9c81127dc4c216dec3c5b738d2d6da18561d8378
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-04-03 19:32:20 -0300

    Fixed another bug when adding repeated elements to FPTree.

--- src/FPGrowth.hs
+++ src/FPGrowth.hs
@@ -7,8 +7,16 @@ minsup = 0.4
 insertTransaction :: [String] -> FPNode -> FPNode
 insertTransaction transaction currentNode 
     | null transaction = currentNode
-    | hasChild (head transaction) (fpchildren currentNode) = FPNode (fpitem currentNode) (fpcount currentNode) [insertTransaction (tail transaction) newElement]
-    | otherwise = FPNode (fpitem currentNode) (fpcount currentNode) (fpchildren currentNode ++ [insertTransaction (tail transaction) newElement])
+    | hasChild (head transaction) oldChildren = FPNode
+                                                    (fpitem currentNode) 
+                                                    (fpcount currentNode)
+                                                    (removeOldElement oldChildren newElement ++ [insertTransaction (tail transaction) newElement])
+    | otherwise = FPNode 
+                        (fpitem currentNode) 
+                        (fpcount currentNode) 
+                        (oldChildren ++ [insertTransaction (tail transaction) newElement])
     where 
         inserted = insertNode (head transaction) currentNode
         newElement = head (fpchildren inserted)
+        oldChildren = fpchildren currentNode
+        removeOldElement oldChildren newElement = [x | x <- oldChildren, fpitem x /= fpitem newElement ]
