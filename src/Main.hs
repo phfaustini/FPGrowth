b5fe00c049bbcfeece011adb7daed2d902a267a5
Pedro Faustini <pedro.faustini@ufabc.edu.br>
2018-04-10 22:20:55 -0300

    Added customised FPTree printing functions.

--- src/Main.hs
+++ src/Main.hs
@@ -6,10 +6,22 @@ import TransactionsReader
 import FPTree -- minsup
 import FPGrowth
 import qualified Data.Map as Map
+import Data.List -- intercalate
 
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile -- ["4 3 1",    "12 3 1",    "23 34 9",   "2","90 1",    "5 1 12"]
 
+
+printFPTree :: FPNode -> String -> String
+printFPTree node spaces
+    | not (null (fpchildren node)) = "\n"++spaces ++ fpitem node ++ " : " ++ show(fpcount node) ++ "  " ++ intercalate (spaces++"    ") [printFPTree x (spaces++"    ") | x <- fpchildren node ]
+    | otherwise = "\n"++spaces ++ fpitem node ++ " : " ++show(fpcount node) ++ "  []"
+
+
+--printTrees :: [FPNode] -> String -> String
+printTrees nodes output
+    | null nodes = output
+    | otherwise = printTrees (tail nodes) (printFPTree (head nodes) " ") ++ "\n--------------------------------\n" ++ output
 
 main::IO ()
 main = do
@@ -36,8 +48,8 @@ main = do
     --putStrLn ""
     let root = FPNode "null" (length transactions) []
     let fptree = buildFPTree (reverse sortedPrunedTransactions) root
-    --print fptree
-    --putStrLn ""
+    putStr (printFPTree fptree " ")
+    putStrLn "\n"
     let prunedFPTree = prune (minsup * fromIntegral (length transactions)) fptree
     --print prunedFPTree
     --putStrLn ""
@@ -47,5 +59,5 @@ main = do
     print cpbs
     putStrLn ""
     let conditionalFPTree = buildConditionalFPTree cpbs headerTablePruned
-    print conditionalFPTree
+    putStrLn (printTrees conditionalFPTree " ")
     putStrLn ""
